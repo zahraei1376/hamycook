2ddd948c7f2c789735586fa49f3d2b63decdecf7
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-25 03:22:20 +0430

    create CityRepository

---
+++ server/hamycook/src/main/java/ir/hamycook/repository/CityRepository.java
@@ -0,0 +1,7 @@
+package ir.hamycook.repository;
+
+import ir.hamycook.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
