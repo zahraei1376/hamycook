e35e40ac9f8e69cb1507a9c7afe320170eabbe03
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-26 21:52:26 +0430

    update CityRepository unit test

--- server/hamycook/src/test/java/ir/hamycook/repository/CityRepositoryTest.java
+++ server/hamycook/src/test/java/ir/hamycook/repository/CityRepositoryTest.java
@@ -2,8 +2,6 @@ package ir.hamycook.repository;
 
 import ir.hamycook.entity.City;
 import ir.hamycook.entity.State;
-import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +9,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -28,47 +23,72 @@ public class CityRepositoryTest {
     @Autowired
     private CityRepository cityRepository;
 
-    @Autowired
-    private StateRepository stateRepository;
-
     public static final String STATE_NAME = "مازندران";
     public static final String BABOL = "بابل";
     public static final String SARI = "ساری";
+
+    @Test
+    public void store_saveCity() {
+        State state = new State(STATE_NAME);
+        State persistedState = testEntityManager.persist(state);
+        City city = new City(BABOL, persistedState);
+        City savedCity = cityRepository.save(city);
+        assertNotNull(savedCity);
+        assertEquals(savedCity.getState(), persistedState);
+        assertEquals(savedCity.getName(), BABOL);
+        assertNotNull(savedCity.getCityId());
+    }
 
     @Test
     public void delete_stateDelete() {
 
         State state = new State(STATE_NAME);
         City city1 = new City(BABOL, state);
+        city1.setState(state);
         City city2 = new City(SARI, state);
-        state.getCities().add(city1);
-        state.getCities().add(city2);
+        city2.setState(state);
 
-        testEntityManager.persist(state);
-        assertEquals(stateRepository.findAll().size(), 1);
-        assertEquals(stateRepository.findById(STATE_NAME).get().getCities().size(), 2);
+        State persistedState = testEntityManager.persist(state);
+        City persistedCity1 = testEntityManager.persist(city1);
+        City persistedCity2 = testEntityManager.persist(city2);
+
         assertEquals(cityRepository.findAll().size(), 2);
 
-        Long cityId = stateRepository.findById(STATE_NAME).get().getCities().get(0).getCityId();
-
-        // remove city in state
-        Optional<State> optionalState = stateRepository.findById(STATE_NAME);
-        State state1 = optionalState.get();
-        for (int i = 0; i < state1.getCities().size(); i++) {
-            if (state1.getCities().get(i).getCityId().equals(cityId)) {
-                state1.getCities().remove(i);
-                break;
-            }
-        }
-        stateRepository.save(state1);
-
-        // delete city
-        cityRepository.deleteById(cityId);
-        assertEquals(stateRepository.findAll().size(), 1);
+        cityRepository.deleteById(persistedCity1.getCityId());
         assertEquals(cityRepository.findAll().size(), 1);
+        Optional<City> optionalCity2 = cityRepository.findById(persistedCity2.getCityId());
+        assertTrue(optionalCity2.isPresent());
+        assertEquals(optionalCity2.get().getState(), persistedState);
+    }
 
-        assertTrue(stateRepository.findById(STATE_NAME).isPresent());
-        assertEquals(stateRepository.findById(STATE_NAME).get().getCities().size(), 1);
+    @Test
+    public void findByName_getCityByName() {
+        State state = new State(STATE_NAME);
+        testEntityManager.persist(state);
+        City city = new City(BABOL, state);
+        testEntityManager.persist(city);
+        Optional<City> optionalCity = cityRepository.findByName(BABOL);
+        assertTrue(optionalCity.isPresent());
+        assertEquals(optionalCity.get().getName(), BABOL);
+        assertEquals(optionalCity.get().getState(), state);
+    }
+
+
+    @Test
+    public void save_updateCity() {
+        State state = new State(STATE_NAME);
+        State persistedState = testEntityManager.persist(state);
+        State state2 = new State(STATE_NAME + STATE_NAME);
+        State persistedState2 = testEntityManager.persist(state2);
+        City city1 = new City(BABOL, state);
+        city1.setState(state);
+        City persistedCity = testEntityManager.persist(city1);
+        persistedCity.setState(state2);
+        cityRepository.save(persistedCity);
+
+        Optional<City> optionalCity = cityRepository.findByName(BABOL);
+        assertTrue(optionalCity.isPresent());
+        assertEquals(optionalCity.get().getState(), persistedState2);
     }
 
 }
