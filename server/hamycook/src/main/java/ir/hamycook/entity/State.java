8697a196bb622b119e82dafcdd2f53c7be156ac7
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-26 21:51:34 +0430

    remove city mapping

--- server/hamycook/src/main/java/ir/hamycook/entity/State.java
+++ server/hamycook/src/main/java/ir/hamycook/entity/State.java
@@ -2,29 +2,21 @@ package ir.hamycook.entity;
 
 import lombok.*;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class State {
+
     @Id
-    //@NotBlank
+    @NotBlank
     @NonNull
     @EqualsAndHashCode.Include
     private String name;
-
-    @OneToMany(mappedBy = "state", cascade = CascadeType.ALL)
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private List<City> cities = new ArrayList<>();
 
 
 }
