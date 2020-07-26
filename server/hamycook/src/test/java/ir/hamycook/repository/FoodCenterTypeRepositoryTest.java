5207670537f8e8f0fca2758469e53aa05c6159c4
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-27 01:08:43 +0430

    update FoodcenterTypeRepository unit test

--- server/hamycook/src/test/java/ir/hamycook/repository/FoodCenterTypeRepositoryTest.java
+++ server/hamycook/src/test/java/ir/hamycook/repository/FoodCenterTypeRepositoryTest.java
@@ -1,12 +1,14 @@
 package ir.hamycook.repository;
 
-import ir.hamycook.entity.FoodCenterType;
+import ir.hamycook.entity.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Time;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -21,19 +23,18 @@ public class FoodCenterTypeRepositoryTest {
     @Autowired
     private FoodCenterTypeRepository foodCenterTypeRepository;
 
+    public static final String FOOD_CENTER_TYPE_NAME = "فست فوت";
 
     @Test
     public void save_storeAFoodCenterType() {
-        String foodCenterTypeName = "فست فوت";
-        FoodCenterType foodCenterType = new FoodCenterType(foodCenterTypeName);
+        FoodCenterType foodCenterType = new FoodCenterType(FOOD_CENTER_TYPE_NAME);
+
         assertEquals(foodCenterTypeRepository.findAll().size(), 0);
 
         FoodCenterType savedFoodCenterType = foodCenterTypeRepository.save(foodCenterType);
+
         assertEquals(foodCenterTypeRepository.findAll().size(), 1);
-
-        assertThat(savedFoodCenterType).hasFieldOrPropertyWithValue("name", foodCenterTypeName);
+        assertThat(savedFoodCenterType).hasFieldOrPropertyWithValue("name", FOOD_CENTER_TYPE_NAME);
     }
-
-
 
 }
