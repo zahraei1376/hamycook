aeff654f87f7ccd201dc8936c8f0fb9873dd9051
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-26 18:30:09 +0430

    create FoodCenterReopsitory

---
+++ server/hamycook/src/main/java/ir/hamycook/repository/FoodCenterTypeRepository.java
@@ -0,0 +1,7 @@
+package ir.hamycook.repository;
+
+import ir.hamycook.entity.FoodCenterType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodCenterTypeRepository extends JpaRepository<FoodCenterType, String> {
+}
