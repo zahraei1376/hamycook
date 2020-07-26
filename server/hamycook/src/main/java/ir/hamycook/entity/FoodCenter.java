f4b0003ef72af74dc1a94dabd0519e7ac36962cb
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-27 01:05:36 +0430

    remove food mapping and add foodCenter mapping

--- server/hamycook/src/main/java/ir/hamycook/entity/FoodCenter.java
+++ server/hamycook/src/main/java/ir/hamycook/entity/FoodCenter.java
@@ -1,60 +1,73 @@
 package ir.hamycook.entity;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class FoodCenter {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @NotBlank(message = "name is required")
+    @NonNull
     private String name;
 
     @NotBlank(message = "address is required")
+    @NonNull
     private String address;
 
     @NotBlank(message = "name is required")
-    @Size(min = 10)
+    @Size(min = 11)
+    @NonNull
     private String phone;
 
+    @NotNull
+    @NonNull
     private Time openTime;
+
+    @NotNull
+    @NonNull
     private Time closeTime;
 
     @ManyToOne
     @JoinColumn(name = "city_id")
+    @NotNull
+    @NonNull
     private City city;
 
     @ManyToOne
     @JoinColumn(name = "owner_id")
+    @NotNull
+    @NonNull
     private User owner;
 
-    @ManyToOne
+    @ManyToMany
     @JoinColumn(name = "food_center_type_id")
-    private FoodCenterType foodCenterType;
+    @NotEmpty
+    private List<FoodCenterType> foodCenterType = new ArrayList<>();
 
-    @OneToMany(mappedBy = "foodCenter")
-    private List<Food> foods;
 
-    public FoodCenter(@NotBlank(message = "name is required") String name,
-                      @NotBlank(message = "address is required") String address,
-                      @NotBlank(message = "name is required") @Size(min = 10) String phone,
-                      Time openTime,
-                      Time closeTime,
-                      City city) {
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-        this.openTime = openTime;
-        this.closeTime = closeTime;
-        this.city = city;
+    public void addFoodCenterType(FoodCenterType foodCenterType) {
+        this.foodCenterType.add(foodCenterType);
     }
+
+    public void removeFoodCenterType(FoodCenterType foodCenterType) {
+        this.foodCenterType.remove(foodCenterType);
+    }
+
 }
