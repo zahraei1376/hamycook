b82d8b3ef81fe87d8159cc5a4a163d1aaa5b8af9
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-27 01:06:19 +0430

    remove foodCenter mapping

--- server/hamycook/src/main/java/ir/hamycook/entity/FoodCenterType.java
+++ server/hamycook/src/main/java/ir/hamycook/entity/FoodCenterType.java
@@ -4,34 +4,16 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Setter
 @Getter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@RequiredArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class FoodCenterType {
 
     @Id
-    @EqualsAndHashCode.Include
     private String name;
 
-    @ManyToMany(mappedBy = "foodCenterType")
-    private List<FoodCenter> foodCenters = new ArrayList<>();
-
-    public FoodCenterType(String name) {
-        this.name = name;
-    }
-
-    public void addFoodCenter(FoodCenter foodCenter) {
-        this.foodCenters.add(foodCenter);
-    }
-
-    public void removeFoodCenter(FoodCenter foodCenter) {
-        this.foodCenters.remove(foodCenter);
-    }
 }
