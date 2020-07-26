d42d08ea814a3f630ed6e45dde955fb41c8c2c73
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-26 21:51:01 +0430

    add findByName in CityRepository

--- server/hamycook/src/main/java/ir/hamycook/repository/CityRepository.java
+++ server/hamycook/src/main/java/ir/hamycook/repository/CityRepository.java
@@ -3,5 +3,10 @@ package ir.hamycook.repository;
 import ir.hamycook.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    Optional<City> findByName(String name);
+
 }
