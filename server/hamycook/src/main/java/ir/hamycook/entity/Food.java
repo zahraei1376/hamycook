6cf5645aebd4785cc328cc0353cf1af30c1e7b0c
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-07-30 16:35:31 +0430

    add some constraint in food entity

--- server/hamycook/src/main/java/ir/hamycook/entity/Food.java
+++ server/hamycook/src/main/java/ir/hamycook/entity/Food.java
@@ -1,34 +1,49 @@
 package ir.hamycook.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Food {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @NonNull
     private String name;
 
     @ManyToOne
     @JoinColumn(name = "food_type")
+    @NonNull
     private FoodType foodType;
 
-    private double price;
-    private double reducedPrice;
+    @Min(0)
+    @NotNull
+    @NonNull
+    private double actualPrice;
 
+    @Min(0)
+    private double discount;
+
+    @Min(0)
+    @NotNull
+    @NonNull
     private Long remainFoodNumber;
 
     @ManyToOne
     @JoinColumn(name = "food_center")
+    @NonNull
     private FoodCenter foodCenter;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinColumn
-    private List<Purchase> purchases;
 }
