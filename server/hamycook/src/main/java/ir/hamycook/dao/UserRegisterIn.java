e66aae2e39166dfd2b763eb7c10f6f3deecb5542
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-01 23:20:01 +0430

    registery input class

--- server/hamycook/src/main/java/ir/hamycook/dao/UserRegisterIn.java
+++ server/hamycook/src/main/java/ir/hamycook/dao/UserRegisterIn.java
@@ -1,4 +1,22 @@
 package ir.hamycook.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
 public class UserRegisterIn {
+
+    @NotBlank(message = "phone number is required")
+    @Size(min = 11, max = 11, message = "phone number must be 11 digits")
+    private String phone;
+
+    @NotBlank(message = "password is required")
+    private String password;
+
+    @NotBlank(message = "fullName is required")
+    private String fullName;
 }
