470712b45907d263fe2fdc6a4e38bfc8d13dba91
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-01 23:17:58 +0430

    disable csrf in spring config

--- server/hamycook/src/main/java/ir/hamycook/config/SecurityConfig.java
+++ server/hamycook/src/main/java/ir/hamycook/config/SecurityConfig.java
@@ -12,7 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .anyRequest().permitAll();
 
     }
