c7f553d4c7ea88992373b987b4531941e741b799
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-02 11:05:09 +0430

    change response message with ErrorResponse class in GlobalExceptionHandler class

--- server/hamycook/src/main/java/ir/hamycook/exception/GlobalExceptionHandler.java
+++ server/hamycook/src/main/java/ir/hamycook/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ir.hamycook.exception;
 
+import ir.hamycook.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -7,21 +8,16 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        Map<String, Object> errors = new HashMap<>();
-        Map<String, String> errorMessages = new HashMap<>();
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse errors = new ErrorResponse();
 
         for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
-            errorMessages.put(fieldError.getField(), fieldError.getDefaultMessage());
+            errors.addErrorMessage(fieldError.getField(), fieldError.getDefaultMessage());
         }
-        errors.put("errors", errorMessages);
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
