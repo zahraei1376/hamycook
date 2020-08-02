c5622a5a0539b84da6ff0ba6a2eb9aa7092f17e1
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-02 22:41:32 +0430

    read custom.properties file

--- server/hamycook/src/main/java/ir/hamycook/HamycookApplication.java
+++ server/hamycook/src/main/java/ir/hamycook/HamycookApplication.java
@@ -2,8 +2,10 @@ package ir.hamycook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:custom.properties")
 public class HamycookApplication {
 
 	public static void main(String[] args) {
